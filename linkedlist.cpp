5a89b656ec92098cf107cee4d771f550e885159e
Isaac Shepherd <isaac.shepherd1@yahoo.com>
2021-10-12 22:30:35 -0500

    setup class prototype in linkedlist.h, and typed out method headers for definitions in linkedlist.cpp'

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,3 +1,42 @@
-/*
- * your comment header here
- */
+/**************
+ * author: Isaac Shepherd
+ * file: linkedlist.cpp
+ **************/
+
+#include "linkedlist.h"
+
+LinkedList::LinkedList(){
+    *head = NULL;
+}
+
+LinkedList::~LinkedList(){
+    clearList();
+}
+
+bool LinkedList::addNode(int, string*){
+
+}
+
+bool LinkedList::deleteNode(int){
+
+}
+
+bool LinkedList::getNode(int, Data*){
+
+}
+
+void LinkedList::printList(bool){
+
+}
+
+int LinkedList::getCount(){
+
+}
+
+bool LinkedList::clearList(){
+
+}
+
+bool LinkedList::exists(int){
+
+}
--- linkedlist.h
+++ linkedlist.h
@@ -1,3 +1,30 @@
-/*
- * your comment header here
- */
+/**************
+ * author: Isaac Shepherd
+ * file: linkedlist.h
+ **************/
+
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include "data.h"
+#include "string"
+using std::string;
+
+class LinkedList{
+    public:
+        LinkedList();
+        ~LinkedList();
+
+        bool addNode(int, string*);
+        bool deleteNode(int);
+        bool getNode(int, Data*);
+        void printList(bool = false);
+        int getCount();
+        bool clearList();
+        bool exists(int);
+    private:
+        Node *head;
+};
+
+#endif
