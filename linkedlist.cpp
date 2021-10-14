ab6ab119b03373bb56142605a016d9b114592476
Isaac Shepherd <isaac.shepherd1@yahoo.com>
2021-10-14 17:34:10 -0500

    defined DeleteNode in linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -17,44 +17,43 @@ bool LinkedList::addNode(int id, string* information){
     bool added = false;
     if(id > 0 and *information != ""){
         Node *current = head;
-        Node *temp;
 
         while(id > current->data.id){
             current = current->next;
         }
         if(id != current->data.id) {
             if (id < current->data.id and current->prev != NULL) { //adds node in list body
-                Node *insertnode = new Node;
-                insertnode->data.id = id;
-                insertnode->data.data = *information;
+                Node *insertNode = new Node;
+                insertNode->data.id = id;
+                insertNode->data.data = *information;
 
-                insertnode->next = current;
-                insertnode->prev = current->prev;
-                current->prev->next = insertnode;
-                current->prev = insertnode;
+                insertNode->next = current;
+                insertNode->prev = current->prev;
+                current->prev->next = insertNode;
+                current->prev = insertNode;
 
                 added = true;
             }
             else if (id < current->data.id and current->prev == NULL) { //adds new head node
-                Node *insertnode = new Node;
-                insertnode->data.id = id;
-                insertnode->data.data = *information;
+                Node *insertNode = new Node;
+                insertNode->data.id = id;
+                insertNode->data.data = *information;
 
-                current->prev = insertnode;
-                insertnode->next = current;
-                insertnode->prev = NULL;
-                head = insertnode;
+                current->prev = insertNode;
+                insertNode->next = current;
+                insertNode->prev = NULL;
+                head = insertNode;
 
                 added = true;
             }
             else if (id > current->data.id and current->next == NULL) { //adds new tail node
-                Node *insertnode = new Node;
-                insertnode->data.id = id;
-                insertnode->data.data = *information;
+                Node *insertNode = new Node;
+                insertNode->data.id = id;
+                insertNode->data.data = *information;
 
-                insertnode->next = NULL;
-                insertnode->prev = current;
-                current->next = insertnode;
+                insertNode->next = NULL;
+                insertNode->prev = current;
+                current->next = insertNode;
 
                 added = true;
             }
@@ -64,7 +63,19 @@ bool LinkedList::addNode(int id, string* information){
 }
 
 bool LinkedList::deleteNode(int id){
+    bool deleted = false;
+    Node *current = head;
 
+    while(id != current->data.id){
+        current = current->next;
+    }
+    if(id == current->data.id){
+        current->prev->next = current->next;
+        current->next->prev = current->prev;
+        delete current;
+        deleted = true;
+    }
+    return deleted;
 }
 
 bool LinkedList::getNode(int id, Data* emptyStruct){
