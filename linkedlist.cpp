4fc8f8e2d50a41ea3fcf7daa82d28b3595de2f97
Isaac Shepherd <isaac.shepherd1@yahoo.com>
2021-10-13 21:43:33 -0500

    worked out some logic and method for adding new node to list under new body and new tail conditions for addNode in linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -6,26 +6,71 @@
 #include "linkedlist.h"
 
 LinkedList::LinkedList(){
-    *head = NULL;
+    head = NULL;
 }
 
 LinkedList::~LinkedList(){
     clearList();
 }
 
-bool LinkedList::addNode(int, string*){
+bool LinkedList::addNode(int id, string* information){ //needs more statements for sorting order
+    bool added = false;                                //needs to pass id and info into data member of node
+    if(id > 0 and *information != ""){
+        Node *current = head;
+        Node *temp;
+
+        while(id > current->data.id){
+            current = current->next;
+            if(current->data.id == id){
+                break;
+            }
+        }
+        if(id < current->data.id and current->prev != NULL){
+            Node *insertnode = new Node;
+            insertnode->data.id = id;
+            insertnode->data.data = *information;
+
+            temp = current->prev;
+            current->prev = insertnode;
+            insertnode->prev = temp;
+            insertnode->next = current;
+            temp->next = insertnode;
+
+            added = true;
+        }
+        if(id > current->data.id and current->next == NULL){
+            Node *insertnode = new Node;
+            insertnode->data.id = id;
+            insertnode->data.data = *information;
+
+            insertnode->next = NULL;
+            insertnode->prev = current;
+            current->next = insertnode;
+        }
+
+        /*
+        head->prev = new;           //layout for adding new head
+        head->prev->next = head;
+        head->prev->prev = NULL;
+        head = new;
+
+        new->next = NULL;           //layout for adding new tail
+        new->prev = current;
+        current->next = new;
+         */
+    }
+    return added;
+}
+
+bool LinkedList::deleteNode(int id){
 
 }
 
-bool LinkedList::deleteNode(int){
+bool LinkedList::getNode(int id, Data* emptyStruct){
 
 }
 
-bool LinkedList::getNode(int, Data*){
-
-}
-
-void LinkedList::printList(bool){
+void LinkedList::printList(bool backwards){
 
 }
 
@@ -37,6 +82,6 @@ bool LinkedList::clearList(){
 
 }
 
-bool LinkedList::exists(int){
+bool LinkedList::exists(int id){
 
 }
